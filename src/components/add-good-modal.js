471af935766e31219808c04a2c60f8067964a581
Мокров Дмитрий <d.mokrov@labmedia.su>
2021-07-29 19:02:04 +0300

    Добавляет валидацию полей форму добавления товара

--- src/components/add-good-modal.js
+++ src/components/add-good-modal.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react';
+import {CountryCities} from '../const';
 import PropTypes from 'prop-types';
 
 class AddGoodModal extends PureComponent {
@@ -6,24 +7,53 @@ class AddGoodModal extends PureComponent {
     super(props);
     this.state = {
       name: '',
+      nameError: '',
       email: '',
       emailError: '',
       count: 0,
       price: 0,
-      delivery: '',
+      delivery: 'no',
+      country: '',
+      city: [],
+      selectAllCities: false,
     };
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleCountChange = this.handleCountChange.bind(this);
     this.handlePriceChange = this.handlePriceChange.bind(this);
+    this.handleDeliveryChange = this.handleDeliveryChange.bind(this);
+    this.handleCountryChange = this.handleCountryChange.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleSelectAllChange = this.handleSelectAllChange.bind(this);
 
+    this.handleNameBlur = this.handleNameBlur.bind(this);
     this.handleEmailBlur = this.handleEmailBlur.bind(this);
+    this.handleCountBlur = this.handleCountBlur.bind(this);
+    this.handlePriceFocus = this.handlePriceFocus.bind(this);
+    this.handlePriceBlur = this.handlePriceBlur.bind(this);
   }
 
   handleNameChange(evt) {
     this.setState({
       name: evt.target.value.trim(),
+    });
+
+    if (this.state.nameError) {
+      this.handleNameBlur(evt);
+    }
+  }
+
+  handleNameBlur(evt) {
+    if (!evt.target.value.trim().length) {
+      this.setState({
+        nameError: 'Имя не может быть пустым',
+      });
+      return;
+    }
+
+    this.setState({
+      nameError: '',
     });
   }
 
@@ -52,19 +82,112 @@ class AddGoodModal extends PureComponent {
 
   handleCountChange(evt) {
     this.setState({
-      count: parseInt(evt.target.value, 10),
+      count: parseInt(evt.target.value, 10) || '',
     });
   }
 
+  handleCountBlur(evt) {
+    evt.target.value = parseInt(evt.target.value, 10) || '';
+  }
+
   handlePriceChange(evt) {
+    evt.target.value = evt.target.value.replace(/[^\d\.]/g, '');
     this.setState({
-      price: parseInt(evt.target.value, 10),
+      price: parseFloat(evt.target.value) || '',
     });
+  }
+
+  handlePriceFocus(evt) {
+    evt.target.value = this.state.price ? this.state.price : '';
+  }
+
+  handlePriceBlur(evt) {
+    evt.target.value = '$' + new Intl.NumberFormat({
+      style: 'currency', currency: 'USD',
+    }).format(this.state.price);
+  }
+
+  handleDeliveryChange(evt) {
+    this.setState({
+      delivery: evt.target.value,
+    });
+
+    if (evt.target.value === 'no') {
+      this.setState({
+        country: '',
+        city: [],
+        selectAllCities: false,
+      });
+    }
+  }
+
+  handleCountryChange(evt) {
+    this.setState({
+      country: evt.target.value,
+      city: [],
+      selectAllCities: false,
+    });
+  }
+
+  handleCityChange(evt) {
+    this.setState((state) => {
+      if (state.city.includes(evt.target.value)) {
+        return {
+          city: state.city.filter((it) => it !== evt.target.value),
+        };
+      }
+
+      return {
+        city: [...state.city, evt.target.value],
+      };
+    });
+
+    if (this.state.city.length === CountryCities[this.state.country].length) {
+      this.setState({
+        selectAllCities: false,
+      });
+    }
+  }
+
+  handleSelectAllChange() {
+    this.setState((prevState) => {
+      if (prevState.selectAllCities) {
+        return {
+          city: [],
+          selectAllCities: !prevState.selectAllCities,
+        };
+      }
+
+      return {
+        city: CountryCities[this.state.country],
+        selectAllCities: !prevState.selectAllCities,
+      };
+    });
+  }
+
+  componentDidUpdate() {
+    if (
+      this.state.country &&
+      this.state.city.length === CountryCities[this.state.country].length &&
+      !this.state.selectAllCities
+    ) {
+      this.setState({
+        selectAllCities: true,
+      });
+    }
   }
 
   render() {
     const {showModal} = this.props;
-    const {emailError} = this.state;
+    const {
+      nameError,
+      emailError,
+      delivery,
+      country,
+      selectAllCities,
+    } = this.state;
+    const hasDelivery = delivery === 'yes' ? true : false;
+    const nameClassname = nameError ? 'input error' : 'input';
     const emailClassname = emailError ? 'input error' : 'input';
 
     return (
@@ -83,13 +206,14 @@ class AddGoodModal extends PureComponent {
               <label htmlFor="name">Name:</label>
               <p>
                 <input
-                  className="input"
+                  className={nameClassname}
                   id="name"
                   type="text"
                   maxLength="15"
                   onChange={this.handleNameChange}
+                  onBlur={this.handleNameBlur}
                 />
-                <span></span>
+                <span>{nameError}</span>
               </p>
             </div>
             <div className="input-wrapper">
@@ -112,6 +236,7 @@ class AddGoodModal extends PureComponent {
                 id="count"
                 type="number"
                 onChange={this.handleCountChange}
+                onBlur={this.handleCountBlur}
               />
             </p>
             <p className="input-wrapper">
@@ -119,50 +244,70 @@ class AddGoodModal extends PureComponent {
               <input
                 className={'input'}
                 id="price"
-                type="number"
+                type="text"
                 onChange={this.handlePriceChange}
+                onFocus={this.handlePriceFocus}
+                onBlur={this.handlePriceBlur}
               />
             </p>
             <fieldset className="add-update-form__fieldset">
               <legend>Delivery:</legend>
               <div className="location">
-                <select className="location__item select">
-                  <option></option>
-                  <option>Страна</option>
-                  <option>Город</option>
+                <select
+                  className="location__item select"
+                  value={delivery}
+                  onChange={this.handleDeliveryChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
                 </select>
-                <p className="location__item radio-checkbox-wrapper">
-                  <label>
-                    <input name="country" value="Россия" type="radio" />
-                    Россия
-                  </label>
-                  <label>
-                    <input name="country" value="США" type="radio" />
-                    США
-                  </label>
-                  <label>
-                    <input name="country" value="Япония" type="radio" />
-                    Япония
-                  </label>
-                </p>
-                <p className="location__item radio-checkbox-wrapper">
-                  <label>
-                    <input name="all" value="all" type="checkbox" />
-                    Select All
-                  </label>
-                  <label>
-                    <input name="Саратов" value="Саратов" type="checkbox" />
-                    Саратов
-                  </label>
-                  <label>
-                    <input name="Москва" value="Москва" type="checkbox" />
-                    Москва
-                  </label>
-                  <label>
-                    <input name="Питер" value="Питер" type="checkbox" />
-                    Питер
-                  </label>
-                </p>
+                {
+                  hasDelivery &&
+                  <p className="location__item radio-checkbox-wrapper">
+                    {
+                      Object.keys(CountryCities).map((country) => (
+                        <label key={country}>
+                          <input
+                            name="country"
+                            value={country}
+                            type="radio"
+                            onChange={this.handleCountryChange}
+                          />
+                          {country}
+                        </label>
+                      ))
+                    }
+                  </p>
+                }
+                {
+                  hasDelivery && country &&
+                  <p className="location__item radio-checkbox-wrapper">
+                    <label>
+                      <input
+                        name="select-all"
+                        value="all"
+                        type="checkbox"
+                        onChange={this.handleSelectAllChange}
+                        checked={selectAllCities}
+                      />
+                      Select All
+                    </label>
+                    {
+                      CountryCities[country].map((city) => (
+                        <label key={city}>
+                          <input
+                            name={city}
+                            value={city}
+                            type="checkbox"
+                            onChange={this.handleCityChange}
+                            checked={this.state.city.includes(city)}
+                          />
+                          {city}
+                        </label>
+                      ))
+                    }
+                  </p>
+                }
               </div>
             </fieldset>
             <button className="btn" type="submit">Add / Update</button>
